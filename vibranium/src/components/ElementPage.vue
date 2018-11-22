441e892fe3bd59b87bdf401d43b9a54c4870f0b4
Ancientae0N <slipfold9@gmail.com>
2018-11-22 17:35:58 +0530

    Added basic layout

--- vibranium/src/components/ElementPage.vue
+++ vibranium/src/components/ElementPage.vue
@@ -1,9 +1,48 @@
 <template>
   <div class="container">
+    <base href="https://raw-dot-custom-elements.appspot.com/FlorianFe/chemical-element-visualisation/v1.0.4/chemical-element-visualisation/">
+    <link rel="import" href="chemical-element-visualisation.html">
+    <header>
 
-  <md-content>
-    {{$route.params.symbol}}
-  </md-content>
+    </header>
+
+    <table width="25%" style="position: absolute; top: 40px;">
+      <tr>
+        <td><chemical-element-visualisation symbol="fe" tooltip="false"></chemical-element-visualisation></td>
+      </tr>
+    </table>
+
+    <table width="50%" border="1" style="position: absolute; top: 50px; left: 27.5%;">
+      <thead>
+      <td>Property</td>
+      <td>Value</td>
+      </thead>
+      <tr>
+        <td>Electronic Configuration</td>
+        <td>1s2 2s2 2p2</td>
+      </tr>
+    </table>
+
+    <table width="15%" style="position: absolute; top: 30px; left: 80%; background: red; text-align: center;">
+      <tr>
+        <td style="font-size: 35px;">
+          C
+        </td>
+      </tr>
+      <tr>
+        <td>Carbon</td>
+      </tr>
+      <tr>
+        <td>40.8</td>
+      </tr>
+    </table>
+    <div class="slidecontainer">
+      <input type="range" min="3" max="10" value="5" class="slider" id="myRange" onchange="set_summary();">
+    </div>
+    <div style="position: absolute; top: 420px;">
+      <p id = "summary_text"></p>
+      some words in the summary
+    </div>
 </div>
 </template>
 
@@ -20,9 +59,8 @@ export default {
   }
 }
 </script>
-
 <style>
-.container: {
-  width:80%;
-}
+  .slidecontainer{
+    top: 100 px;
+  }
 </style>
