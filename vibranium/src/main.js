967c6d911c08bb7e202875e2f8f8c80bc8cf1708
Ancientae0N <slipfold9@gmail.com>
2018-11-22 22:07:06 +0530

    Added vuetify

--- vibranium/src/main.js
+++ vibranium/src/main.js
@@ -7,11 +7,14 @@ import VueFlex from "vue-flex";
 // Also namespaced to avoid collisions.
 import "vue-flex/dist/vue-flex.css";
 import VueMaterial from 'vue-material'
+import Vuetify from 'vuetify'
+
 
 import App from './App'
 import router from './router'
 import VueRouter from 'vue-router'
 import isotope from 'vueisotope'
+import 'vuetify/dist/vuetify.min.css'
 
 Vue.config.productionTip = false
 
@@ -20,6 +23,7 @@ Vue.use(VueFlex);
 Vue.config.productionTip = false;
 Vue.use(VueMaterial);
 Vue.use(VueRouter);
+Vue.use(Vuetify);
 
 
 /* eslint-disable no-new */
