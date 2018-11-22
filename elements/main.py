b117d48e92846462f4fc202f54a14d4bf6ee1e1d
Varun Vora <varunvora@live.com>
2018-11-22 18:06:59 +0530

    updated async call

--- elements/main.py
+++ elements/main.py
@@ -1,13 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_cors import CORS, cross_origin
 from elements.tfidf import ExtractiveSummarizer_tfidf
 import json
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
 text_summarizer = ExtractiveSummarizer_tfidf(corpus = "clean_dataset")
 with open("element_data.json", "r") as f:
     element_data = json.load(f)
 
+@app.route("/")
+@cross_origin()
+def first():
+    return render_template("element.html")
+
 
 @app.route("/summary/<element>/<length>")
+@cross_origin(support_credentials=True)
 def summary(element, length):
     length = int(length)
     assert type(element) == str  and len(element) < 3
@@ -18,11 +26,15 @@ def summary(element, length):
 
 
 @app.route("/data/<element>")
+@cross_origin()
 def get_element_data(element):
-    assert type(element) == str and  1 <= len(element) < 3
-    element = element.lower()
-    print(element_data[element])
-    return json.dumps(element_data[element])
+    if element == "all":
+        return json.dumps(element_data)
+    else:
+        element = element.lower()
+        return json.dumps(element_data[element])
+
+
 
 
 if __name__ == "__main__":
