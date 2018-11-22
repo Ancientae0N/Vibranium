c505b0910544f3c9070d86abeaeb7c963117ea0c
Sumeet Padavala <sumeet.padavala@gmail.com>
2018-11-22 19:07:55 +0530

    Fix the slider issue(mostly)

--- elements/main.py
+++ elements/main.py
@@ -1,24 +1,26 @@
-from flask import Flask, render_template
-from flask_cors import CORS, cross_origin
-from elements.tfidf import ExtractiveSummarizer_tfidf
 import json
+
+from flask import Flask, render_template
+from flask_cors import CORS
+
+from tfidf import ExtractiveSummarizer_tfidf
+
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
-text_summarizer = ExtractiveSummarizer_tfidf(corpus = "clean_dataset")
+text_summarizer = ExtractiveSummarizer_tfidf(corpus="clean_dataset")
 with open("element_data.json", "r") as f:
     element_data = json.load(f)
 
+
 @app.route("/")
-@cross_origin()
 def first():
     return render_template("element.html")
 
 
 @app.route("/summary/<element>/<length>")
-@cross_origin(support_credentials=True)
 def summary(element, length):
     length = int(length)
-    assert type(element) == str  and len(element) < 3
+    assert type(element) == str and len(element) < 3
     assert 2 < length
     element = element.lower()
     text = element_data[element]["text"]
@@ -26,15 +28,12 @@ def summary(element, length):
 
 
 @app.route("/data/<element>")
-@cross_origin()
 def get_element_data(element):
     if element == "all":
         return json.dumps(element_data)
     else:
         element = element.lower()
         return json.dumps(element_data[element])
-
-
 
 
 if __name__ == "__main__":
